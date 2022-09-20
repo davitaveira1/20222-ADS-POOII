d35fc145f378d95b2216c4e20bc3695596d54eb2
Davi Taveira <davitaveira@gmail.com>
2022-09-20 19:50:00 -0300

    aula-06 2009

--- 20222-POOII-producao-Aula06/src/Telas/TelaGestaoClientes.java
+++ 20222-POOII-producao-Aula06/src/Telas/TelaGestaoClientes.java
@@ -4,17 +4,27 @@
  */
 package Telas;
 
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author Davi
  */
 public class TelaGestaoClientes extends javax.swing.JFrame {
 
+    DefaultTableModel modelo;
+
     /**
      * Creates new form TelaGestaoClientes
      */
     public TelaGestaoClientes() {
         initComponents();
+        getTableModel();
+        modelo.addRow(new Object[]{"Davi","Taveira",38});
+    }
+
+    public void getTableModel() {
+        modelo = (DefaultTableModel) tb_clientes.getModel();
     }
 
     /**
@@ -29,28 +39,28 @@ public class TelaGestaoClientes extends javax.swing.JFrame {
         jTabbedPane1 = new javax.swing.JTabbedPane();
         jPanel1 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        tb_clientes = new javax.swing.JTable();
         jPanel2 = new javax.swing.JPanel();
         jPanel3 = new javax.swing.JPanel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
+        bt_cli_novo = new javax.swing.JButton();
+        bt_cli_editar = new javax.swing.JButton();
+        bt_cli_excluir = new javax.swing.JButton();
         jPanel4 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
+        c_cli_nome = new javax.swing.JTextField();
+        c_cli_sobrenome = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
-        jTextField3 = new javax.swing.JTextField();
+        c_cli_idade = new javax.swing.JTextField();
         jPanel5 = new javax.swing.JPanel();
-        jButton4 = new javax.swing.JButton();
-        jButton5 = new javax.swing.JButton();
+        bt_cli_salvar = new javax.swing.JButton();
+        bt_cli_cancelar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jPanel1.setLayout(new java.awt.BorderLayout());
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        tb_clientes.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null},
                 {null, null, null},
@@ -69,7 +79,7 @@ public class TelaGestaoClientes extends javax.swing.JFrame {
                 return types [columnIndex];
             }
         });
-        jScrollPane1.setViewportView(jTable1);
+        jScrollPane1.setViewportView(tb_clientes);
 
         jPanel1.add(jScrollPane1, java.awt.BorderLayout.CENTER);
 
@@ -90,14 +100,14 @@ public class TelaGestaoClientes extends javax.swing.JFrame {
 
         jPanel3.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.CENTER, 50, 5));
 
-        jButton1.setText("Novo");
-        jPanel3.add(jButton1);
+        bt_cli_novo.setText("Novo");
+        jPanel3.add(bt_cli_novo);
 
-        jButton2.setText("Editar");
-        jPanel3.add(jButton2);
+        bt_cli_editar.setText("Editar");
+        jPanel3.add(bt_cli_editar);
 
-        jButton3.setText("Excluir");
-        jPanel3.add(jButton3);
+        bt_cli_excluir.setText("Excluir");
+        jPanel3.add(bt_cli_excluir);
 
         jPanel4.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)), "Dados"));
 
@@ -119,9 +129,9 @@ public class TelaGestaoClientes extends javax.swing.JFrame {
                     .addComponent(jLabel1))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jTextField1)
-                    .addComponent(jTextField2)
-                    .addComponent(jTextField3, javax.swing.GroupLayout.DEFAULT_SIZE, 348, Short.MAX_VALUE))
+                    .addComponent(c_cli_nome)
+                    .addComponent(c_cli_sobrenome)
+                    .addComponent(c_cli_idade, javax.swing.GroupLayout.DEFAULT_SIZE, 348, Short.MAX_VALUE))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel4Layout.setVerticalGroup(
@@ -130,23 +140,23 @@ public class TelaGestaoClientes extends javax.swing.JFrame {
                 .addGap(11, 11, 11)
                 .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(c_cli_nome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(c_cli_sobrenome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel3)
-                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(c_cli_idade, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(20, Short.MAX_VALUE))
         );
 
-        jButton4.setText("Salvar");
-        jPanel5.add(jButton4);
+        bt_cli_salvar.setText("Salvar");
+        jPanel5.add(bt_cli_salvar);
 
-        jButton5.setText("Cancelar");
-        jPanel5.add(jButton5);
+        bt_cli_cancelar.setText("Cancelar");
+        jPanel5.add(bt_cli_cancelar);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -155,7 +165,7 @@ public class TelaGestaoClientes extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(21, 21, 21)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jTabbedPane1)
+                    .addComponent(jTabbedPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 509, Short.MAX_VALUE)
                     .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jPanel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jPanel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
@@ -215,11 +225,14 @@ public class TelaGestaoClientes extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
-    private javax.swing.JButton jButton5;
+    private javax.swing.JButton bt_cli_cancelar;
+    private javax.swing.JButton bt_cli_editar;
+    private javax.swing.JButton bt_cli_excluir;
+    private javax.swing.JButton bt_cli_novo;
+    private javax.swing.JButton bt_cli_salvar;
+    private javax.swing.JTextField c_cli_idade;
+    private javax.swing.JTextField c_cli_nome;
+    private javax.swing.JTextField c_cli_sobrenome;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -230,9 +243,6 @@ public class TelaGestaoClientes extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel5;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTabbedPane jTabbedPane1;
-    private javax.swing.JTable jTable1;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
+    private javax.swing.JTable tb_clientes;
     // End of variables declaration//GEN-END:variables
 }
